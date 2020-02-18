e1ed75d0d5e817e357a0b7f2e40a9faae34d5eeb
Ubuntu <ubuntu@ip-172-31-41-146.ec2.internal>
2020-02-18 10:13:02 +0000

    added delete and edit functionality

--- start/routes.js
+++ start/routes.js
@@ -22,7 +22,18 @@ Route.get('/posts', '/PostController.index')
 
 Route.get('/posts/add', 'PostController.add')
 
+Route.get('/posts/edit/:id', 'PostController.edit')
+
 Route.get('/posts/:id', "PostController.details")
+
+Route.post('/posts', 'PostController.store')
+
+Route.put('/posts/:id', 'PostController.update')
+
+Route.delete('/posts/:id', 'PostController.destroy')
+
+
+
 
 // Route.get('/test', () => 'Hello World')
 
